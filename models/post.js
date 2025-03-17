06b2c4a306af542db8fd0fb2b2da08d59c9ad3c7
httpaju <165481606+httpaju@users.noreply.github.com>
2025-03-17 22:45:58 +0530

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,11 +1,18 @@
-
 const mongoose = require('mongoose');
 
-const postSchema = {
-    title: String,
-    content: String
-};
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-const Post = mongoose.model('Post', postSchema);
-
-module.exports = Post;
+module.exports = mongoose.model('Post', postSchema);
